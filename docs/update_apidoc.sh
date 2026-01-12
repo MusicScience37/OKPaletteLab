a52e72a9915c2371a44765a75763c012e92581be
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-01-12 09:06:49 +0000

    :wrench: Run sphinx-apidoc in virtual environment.

--- docs/update_apidoc.sh
+++ docs/update_apidoc.sh
@@ -4,4 +4,4 @@ set -e
 
 cd $(dirname $0)
 
-sphinx-apidoc -H "API Reference" -f -e -q -o ./source/api ../ok_palette_lab
+poetry run sphinx-apidoc -H "API Reference" -f -e -q -o ./source/api ../ok_palette_lab
