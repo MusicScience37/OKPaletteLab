4bffd8d6c3818125d5fe145ca45c48ef23030dfd
Kenta Kabashima <kenta_program37@hotmail.co.jp>
2026-01-31 13:52:07 +0000

    :recycle: Decrease resolutions of figures.

--- ok_palette_lab_tools/test_data/perlin_noise.py
+++ ok_palette_lab_tools/test_data/perlin_noise.py
@@ -10,8 +10,8 @@ def generate_perlin_noise_data() -> tuple[numpy.ndarray, numpy.ndarray, numpy.nd
     Returns:
         tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]: x, y, z data.
     """
-    x = numpy.linspace(0.0, 8.0, 401)
-    y = numpy.linspace(0.0, 8.0, 401)
+    x = numpy.linspace(0.0, 8.0, 201)
+    y = numpy.linspace(0.0, 8.0, 201)
     x_grid, y_grid = numpy.meshgrid(x, y)
     z = numpy.vectorize(lambda x, y: noise.pnoise2(x, y, octaves=6))(x_grid, y_grid)
     return x, y, z
--- ok_palette_lab_tools/test_data/spiral_pattern.py
+++ ok_palette_lab_tools/test_data/spiral_pattern.py
@@ -11,12 +11,12 @@ def generate_spiral_pattern_data() -> (
     Returns:
         tuple[numpy.ndarray, numpy.ndarray, numpy.ndarray]: x, y, z data.
     """
-    x = numpy.linspace(-2.0, 2.0, 401)
-    y = numpy.linspace(-2.0, 2.0, 401)
+    x = numpy.linspace(-2.0, 2.0, 200)
+    y = numpy.linspace(-2.0, 2.0, 200)
     x_grid, y_grid = numpy.meshgrid(x, y)
 
     r = numpy.sqrt(x_grid**2 + y_grid**2)
     t = numpy.arctan2(y_grid, x_grid)
-    z = numpy.sin(3 * t + 10 * r) * numpy.exp(-(r**2))
+    z = numpy.sin(2 * t + 5 * r) * numpy.exp(-0.7 * (r**2))
 
     return x, y, z
